a1ca157fba55f320a71d376f7c7c35a868556c98
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-03-05 14:16:29 +0100

    delete  useless {data} in response

--- controllers/users.js
+++ controllers/users.js
@@ -4,161 +4,159 @@ const gravatar = require("gravatar");
 const Jimp = require("jimp");
 const fs = require("fs/promises");
 const path = require("path");
-const { nanoid } = require("nanoid");
+const {nanoid} = require("nanoid");
 
-const { User } = require("../models/user");
-const { ctrlWrapper, HttpError, sendEmail } = require("../helpers/index");
+const {User} = require("../models/user");
+const {ctrlWrapper, HttpError, sendEmail} = require("../helpers/index");
 
 require("dotenv").config();
 const secret = process.env.SECRET_KEY;
 const baseUrl = process.env.BASE_URL;
 
 const register = async (req, res) => {
-  const { email, password, name } = req.body;
-  const user = await User.findOne({ email });
+    const {email, password, name} = req.body;
+    const user = await User.findOne({email});
 
-  if (user) {
-    throw HttpError(409, "Email in use");
-  }
-  const hashPassword = await bcrypt.hash(password, 10);
-  avatarURL = gravatar.url(email);
-  const verificationToken = nanoid();
+    if (user) {
+        throw HttpError(409, "Email in use");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
+    const verificationToken = nanoid();
 
-  const newUser = await User.create({
-    ...req.body,
-    password: hashPassword,
-    avatarURL,
-    verificationToken,
-  });
-  const verifyEmail = {
-    to: email,
-    subject: "Verify your email",
-    html: `<a tarhet="_blank" href="${baseUrl}/api/users/verify/${verificationToken}">Click to verify email</a>`,
-  };
-  await sendEmail(verifyEmail);
+    const newUser = await User.create({
+        ...req.body,
+        password: hashPassword,
+        avatarURL,
+        verificationToken,
+    });
+    const verifyEmail = {
+        to: email,
+        subject: "Verify your email",
+        html: `<a tarhet="_blank" href="${baseUrl}/api/users/verify/${verificationToken}">Click to verify email</a>`,
+    };
+    await sendEmail(verifyEmail);
 
-  const id = newUser._id;
-  const token = jwt.sign({ id }, secret, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(id, { token });
+    const id = newUser._id;
+    const token = jwt.sign({id}, secret, {expiresIn: "23h"});
+    await User.findByIdAndUpdate(id, {token});
 
-  res.status(201).json({
-    data: {
-      token,
-      name,
-      email,
-      avatarURL,
-    },
-  });
+    res.status(201).json({
+        token,
+        name,
+        email,
+        avatarURL,
+
+    });
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+    const {email, password} = req.body;
 
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw HttpError(401, "Email or password wrong!");
-  }
-  const compareResult = await bcrypt.compare(password, user.password);
+    const user = await User.findOne({email});
+    if (!user) {
+        throw HttpError(401, "Email or password wrong!");
+    }
+    const compareResult = await bcrypt.compare(password, user.password);
 
-  if (!compareResult) {
-    throw HttpError(401, "Email or password wrong!");
-  }
+    if (!compareResult) {
+        throw HttpError(401, "Email or password wrong!");
+    }
 
-  const id = user._id;
-  const token = jwt.sign({ id }, secret, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(id, { token });
+    const id = user._id;
+    const token = jwt.sign({id}, secret, {expiresIn: "23h"});
+    await User.findByIdAndUpdate(id, {token});
 
-  res.status(201).json({
-    data: {
-      token,
-      name: user.name,
-      email,
-    },
-  });
+    res.status(201).json({
+            token,
+            name: user.name,
+            email,
+
+    });
 };
 
 const logout = async (req, res) => {
-  const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204).end();
+    const {_id} = req.user;
+    await User.findByIdAndUpdate(_id, {token: ""});
+    res.status(204).end();
 };
 
 const getCurrent = async (req, res) => {
-  const { email, wallet, avatarURL, token } = req.user;
-  res.json({
-    user: { email, wallet, avatarURL, token },
-  });
+    const {email, wallet, avatarURL, token} = req.user;
+    res.json({
+        email, wallet, avatarURL, token
+    });
 };
 
 const updateAvatar = async (req, res) => {
-  const { _id } = req.user;
+    const {_id} = req.user;
 
-  const avatarDir = path.join(__dirname, "../public/avatars");
+    const avatarDir = path.join(__dirname, "../public/avatars");
 
-  const { path: teamUpload, originalname } = req.file;
-  const filename = `${_id}_${originalname}`;
+    const {path: teamUpload, originalname} = req.file;
+    const filename = `${_id}_${originalname}`;
 
-  const resultUpload = path.join(avatarDir, originalname);
+    const resultUpload = path.join(avatarDir, originalname);
 
-  await Jimp.read(teamUpload)
-    .then((lenna) => lenna.resize(250, 250).write(resultUpload))
-    .catch((e) => console.log(e));
+    await Jimp.read(teamUpload)
+        .then((lenna) => lenna.resize(250, 250).write(resultUpload))
+        .catch((e) => console.log(e));
 
-  await fs.unlink(teamUpload);
+    await fs.unlink(teamUpload);
 
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
-  res.json({
-    avatarURL,
-  });
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, {avatarURL});
+    res.json({
+        avatarURL,
+    });
 };
 
 const verify = async (req, res) => {
-  const { verificationToken } = req.params;
-  const user = await User.findOne({ verificationToken });
-  if (!user) {
-    throw HttpError(404, "User not found");
-  }
+    const {verificationToken} = req.params;
+    const user = await User.findOne({verificationToken});
+    if (!user) {
+        throw HttpError(404, "User not found");
+    }
 
-  await User.findByIdAndUpdate(user._id, {
-    verify: true,
-    verificationToken: "",
-  });
+    await User.findByIdAndUpdate(user._id, {
+        verify: true,
+        verificationToken: "",
+    });
 
-  res.json({
-    message: "Verification successful",
-  });
+    res.json({
+        message: "Verification successful",
+    });
 };
 
 const resendVerifyEmail = async (req, res) => {
-  const { email } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw HttpError(404, "User not found");
-  }
+    const {email} = req.body;
+    const user = await User.findOne({email});
+    if (!user) {
+        throw HttpError(404, "User not found");
+    }
 
-  if (user.verify) {
-    throw HttpError(400, "Verification has already been passed");
-  }
+    if (user.verify) {
+        throw HttpError(400, "Verification has already been passed");
+    }
 
-  const verifyEmail = {
-    to: email,
-    subject: "Verify your email",
-    html: `<a target="_blank" href="${baseUrl}/api/users/verify/${user.verificationToken}">Click to verify email</a>`,
-  };
-  await sendEmail(verifyEmail);
+    const verifyEmail = {
+        to: email,
+        subject: "Verify your email",
+        html: `<a target="_blank" href="${baseUrl}/api/users/verify/${user.verificationToken}">Click to verify email</a>`,
+    };
+    await sendEmail(verifyEmail);
 
-  res.json({
-    message: "Verification email sent",
-  });
+    res.json({
+        message: "Verification email sent",
+    });
 };
 
 module.exports = {
-  register: ctrlWrapper(register),
-  login: ctrlWrapper(login),
-  getCurrent: ctrlWrapper(getCurrent),
-  logout: ctrlWrapper(logout),
-  updateAvatar: ctrlWrapper(updateAvatar),
-  verify: ctrlWrapper(verify),
-  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
+    verify: ctrlWrapper(verify),
+    resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 };
