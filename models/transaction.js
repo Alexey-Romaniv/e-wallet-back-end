36ef2c591fa0c29ac80b050bc14a0d7122469d28
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-03-06 09:14:28 +0100

    fix model transactions

--- models/transaction.js
+++ models/transaction.js
@@ -1,51 +1,55 @@
-const { Schema, model } = require("mongoose");
+const {Schema, model} = require("mongoose");
 const Joi = require("joi");
 
-const { handleMongooseError } = require("../helpers");
+const {handleMongooseError} = require("../helpers");
 
 const types = ["+", "-"];
 
 const transactionSchema = new Schema(
-  {
-    date: {
-      type: Date,
-      required: true,
+    {
+        date: {
+            type: Date,
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: types,
+            required: true,
+        },
+        category: {
+            type: String,
+        },
+        comment: {type: String},
+        sum: {
+            type: Number,
+            required: true,
+        },
+        wallet: {
+            type: Number,
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+        },
     },
-    type: {
-      type: String,
-      enum: types,
-      required: true,
-    },
-    category: {
-      type: String,
-    },
-    comment: { type: String },
-    sum: {
-      type: Number,
-      required: true,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  },
-  { versionKey: false, timestamps: false }
+    {versionKey: false, timestamps: false}
 );
 
 transactionSchema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
-  date: Joi.date().required(),
-  type: Joi.string()
-    .valid(...types)
-    .required(),
-  category: Joi.string(),
-  comment: Joi.string(),
-  sum: Joi.number().required(),
+    date: Joi.date().required(),
+    type: Joi.string()
+        .valid(...types)
+        .required(),
+    category: Joi.string(),
+    comment: Joi.string(),
+    sum: Joi.number().required(),
 });
 
 const Transaction = model("transaction", transactionSchema);
 module.exports = {
-  Transaction,
-  addSchema,
+    Transaction,
+    addSchema,
 };
